0968725d9555c23c164df3447b1e882c5de5699b
rramsaro <rramsaro@ggc.edu>
2013-03-09 14:21:35 -0500

    Started creating frame.

---
+++ src/MemeMaker.java
@@ -0,0 +1,24 @@
+import java.awt.CardLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class MemeMaker {
+	
+	private static JFrame frame;
+	
+	private CardLayout layout;
+	
+	private JPanel pnlEdit;
+	private JPanel pnlSetup;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
