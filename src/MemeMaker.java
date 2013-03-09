c7a6fae7bcf07b7477df67437d334ab55465201b
rramsaro <rramsaro@ggc.edu>
2013-03-09 14:47:53 -0500

    Added JMenu and listener

--- src/MemeMaker.java
+++ src/MemeMaker.java
@@ -1,6 +1,10 @@
 import java.awt.CardLayout;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class MemeMaker {
@@ -12,6 +16,9 @@ public class MemeMaker {
 	private JPanel pnlEdit;
 	private JPanel pnlSetup;
 	
+	public static JMenuItem menuFileNew, menuFileExport, menuFileExit;
+	public static JMenuItem menuHelpAbout;
+	
 	public static final String SCREEN_EDIT = "Edit Screen";
 	public static final String SCREEN_SETUP = "Setup Screen";
 	
@@ -22,11 +29,56 @@ public class MemeMaker {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(800,600);
 		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setJMenuBar(createMenuBar());
 		
 		createGuiComponenets();
 		frame.add(pnlEdit, SCREEN_EDIT);
 		frame.add(pnlSetup, SCREEN_SETUP);
 		frame.setVisible(true);
+	}
+
+	private JMenuBar createMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		
+		// Menus in menu bar
+		JMenu menuFile, menuHelp;
+		
+		// Create file menu
+		menuFile = new JMenu("File");
+		menuFile.setMnemonic(KeyEvent.VK_F);
+		menuFile.getAccessibleContext().setAccessibleDescription("File Menu");
+		menuBar.add(menuFile);
+		
+		// File menu items
+		menuFileNew = new JMenuItem("New Meme", KeyEvent.VK_N);
+		menuFileNew.addActionListener(new MemeMakerListener());
+		menuFileNew.getAccessibleContext().setAccessibleDescription("Create a new meme from an image");
+		menuFile.add(menuFileNew);
+		
+		menuFileExport = new JMenuItem("Export/Save Meme", KeyEvent.VK_E);
+		menuFileExport.addActionListener(new MemeMakerListener());
+		menuFileExport.getAccessibleContext().setAccessibleDescription("Save the meme as an image");
+		menuFile.add(menuFileExport);
+		
+		menuFileExit = new JMenuItem("Exit", KeyEvent.VK_X);
+		menuFileExit.addActionListener(new MemeMakerListener());
+		menuFileExit.getAccessibleContext().setAccessibleDescription("Exit the program");
+		menuFile.add(menuFileExit);
+		
+		// create help menu
+		menuHelp = new JMenu("Help");
+		menuHelp.setMnemonic(KeyEvent.VK_H);
+		menuHelp.getAccessibleContext().setAccessibleDescription("Help Menu");
+		menuBar.add(menuHelp);
+		
+		// help menu items
+		menuHelpAbout = new JMenuItem("About", KeyEvent.VK_A);
+		menuHelpAbout.addActionListener(new MemeMakerListener());
+		menuHelpAbout.getAccessibleContext().setAccessibleDescription("About this program");
+		menuHelp.add(menuHelpAbout);
+		
+		return menuBar;
 	}
 
 	private void createGuiComponenets() {
@@ -50,8 +102,7 @@ public class MemeMaker {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new MemeMaker();
 	}
 
 }
