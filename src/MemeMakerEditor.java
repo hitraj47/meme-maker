4b8f853143d7b94b03b56568355a61c6c1727817
rramsaro <rramsaro@ggc.edu>
2013-03-09 19:31:00 -0500

    Rejiggered menu

--- src/MemeMakerEditor.java
+++ src/MemeMakerEditor.java
@@ -1,16 +1,12 @@
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 
 public class MemeMakerEditor extends JPanel {
 
@@ -60,34 +56,5 @@ public class MemeMakerEditor extends JPanel {
 			System.err.println("Couldn't find file: " + path);
 			return null;
 		}
-	}
-
-	/**
-	 * Create the GUI and show it. For thread safety, this method should be
-	 * invoked from the event dispatch thread.
-	 */
-	private static void createAndShowGUI() {
-		// Create and set up the window.
-		JFrame frame = new JFrame("TabbedPaneDemo");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		// Add content to the window.
-		frame.add(new MemeMakerEditor(), BorderLayout.CENTER);
-
-		// Display the window.
-		frame.pack();
-		frame.setVisible(true);
-	}
-
-	public static void main(String[] args) {
-		// Schedule a job for the event dispatch thread:
-		// creating and showing this application's GUI.
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				// Turn off metal's use of bold fonts
-				UIManager.put("swing.boldMetal", Boolean.FALSE);
-				createAndShowGUI();
-			}
-		});
 	}
 }
